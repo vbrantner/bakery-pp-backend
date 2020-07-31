b15619644d60789f172e3f06a397d3a99a891e83
Vinzenz <vinzenz@MacBook-Pro-2.local>
2020-07-31 17:25:37 +0200

    remove whitenoise gzip

--- api/settings.py
+++ api/settings.py
@@ -162,6 +162,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
