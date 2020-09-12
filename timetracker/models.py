c3a818b235e6b34f685a6bfad63965b6a42904ad
Vinzenz Brantner <vinzenzbrantner@me.com>
2020-09-12 10:15:03 +0200

    add auto datetime

--- timetracker/models.py
+++ timetracker/models.py
@@ -16,6 +16,8 @@ class Attandance(models.Model):
     time = models.TimeField()
     date = models.DateField()
     gsheet_type = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return "{}_{}-{}_{}".format(self.employee.name, self.date, self.time, self.gsheet_type)
